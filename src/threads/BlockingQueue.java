b14d1ad420d961aa055c5b26fbf336565727b851
victor110298 <44604893+victor110298@users.noreply.github.com>
2019-03-16 18:31:53 +0200

    Create class BlockingQueue

---
+++ src/threads/BlockingQueue.java
@@ -0,0 +1,17 @@
+package threads;
+
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class BlockingQueue {private static java.util.concurrent.BlockingQueue<String> blockingQueue=new PriorityBlockingQueue<>();
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            try {
+                System.out.println(blockingQueue.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        Thread.sleep(3000);
+        new Thread(()-> System.out.println(blockingQueue.add("something"))).start();
+    }
+}
